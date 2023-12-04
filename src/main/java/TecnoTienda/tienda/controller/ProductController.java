a58478a08d747efc3a6424013afbb8630b4eb93c
Nicolás Muros <nicomuros@gmail.com>
2023-12-04 00:35:49 -0300

    CORS

--- src/main/java/TecnoTienda/tienda/controller/ProductController.java
+++ src/main/java/TecnoTienda/tienda/controller/ProductController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class ProductController {
     
     @Autowired
--- src/main/java/TecnoTienda/tienda/controller/UserController.java
+++ src/main/java/TecnoTienda/tienda/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController("/api")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
